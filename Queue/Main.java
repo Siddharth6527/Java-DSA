198b900522c153dfdad8facbc23eafcc910b76ba
Siddharth <siddharth6527@gmail.com>
2023-11-05 06:33:02 +0530

    Added Queue using Linked List

--- Queue/Main.java
+++ Queue/Main.java
@@ -282,3 +282,83 @@
 // }
 
 // }
+
+/////////////////////////////////////////////////////////////
+// Queue implementation using Linked List
+// a linked list (ll) node to store a queue entry
+// class QNode {
+// int key;
+// QNode next;
+
+// // constructor to create a new linked list node
+// public QNode(int key) {
+// this.key = key;
+// this.next = null;
+// }
+// }
+
+// // a class to represent a queue
+// // The queue, front stores the front node of ll and
+// // rear stores the last node of ll
+// class Queue {
+// QNode front, rear;
+
+// public Queue() {
+// this.front = this.rear = null;
+// }
+
+// // method to add an key to the queue
+// void enqueue(int key) {
+// // create a new ll node
+// QNode temp = new QNode(key);
+
+// // if queue is empty, then new node is front and
+// // rear both
+// if (this.rear == null) {
+// this.front = this.rear = temp;
+// return;
+// }
+
+// // add the new node at the end of queue
+// // and change rear
+// this.rear.next = temp;
+// this.rear = temp;
+// }
+
+// // method to remove an key from queue
+// void dequeue() {
+// // if queue is empty, return NULL
+// if (this.front == null) {
+// return;
+// }
+
+// // store previous front and move front one node
+// // ahead
+// // QNode temp = this.front;
+// this.front = this.front.next;
+
+// // if front becomes null, then change rear also as NULL
+// if (this.front == null)
+// this.rear = null;
+// }
+// }
+
+// // driver code
+// public class Main {
+// public static void main(String[] args) {
+// Queue q = new Queue();
+// q.enqueue(10);
+// q.enqueue(20);
+// q.dequeue();
+// q.dequeue();
+// q.enqueue(30);
+// q.enqueue(40);
+// q.enqueue(50);
+// q.dequeue();
+
+// System.out.println("Queue Front : " + ((q.front != null) ? (q.front).key :
+// -1));
+// System.out.println("Queue Rear : " + ((q.rear != null) ? (q.rear).key : -1));
+
+// }
+// }
