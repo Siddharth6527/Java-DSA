778c41afd7ea09db8fd19fe2288111e31024b154
Siddharth <siddharth6527@gmail.com>
2023-12-04 00:07:15 +0530

    Added Rotations (AVL Trees)

--- AVL Trees/AVL.java
+++ AVL Trees/AVL.java
@@ -158,3 +158,207 @@
 // Complexity Analysis:
 // Time Complexity: O(log (n)), for insertion
 // Auxiliary Space: O(1)
+
+///////////////////////////////////////////////////////////////////////
+// Insertion in AVL Tree (UDEMY CODE)
+class Node {
+  int data, height;
+  Node left, right;
+
+  Node(int d) {
+    data = d;
+    height = 1;
+    // starting height from 1 onwards
+    left = right = null;
+  }
+}
+
+class Main {
+  static Node root;
+
+  // function to find the height of the node
+  // the height of p node is maximum of two child's height
+  public static int NodeHeight(Node root) {
+    int hl, hr;
+    hl = (root != null && root.left != null) ? root.left.height : 0;
+    hr = (root != null && root.right != null) ? root.right.height : 0;
+
+    return hl > hr ? hl + 1 : hr + 1;
+  }
+
+  public static int BalanceFactor(Node root) {
+    int hl = 0, hr;
+    hl = (root != null && root.left != null) ? root.left.height : 0;
+    hr = (root != null && root.right != null) ? root.right.height : 0;
+
+    return hl - hr;
+  }
+
+  public static Node LLRotation(Node p) {
+    // height of plr will remain same
+    // and the height of pl and p will change
+
+    // take two extra pointers
+    // left child of node P - pl
+    // right of the (left node of P) -plr
+
+    Node pl = p.left;
+    Node plr = pl.right;
+
+    // make the right child of pl that is plr
+    // the left child of p
+    p.left = plr;
+    // and make p as the right child of pl
+    pl.right = p;
+
+    // first make changes in the height of p (lower)
+    p.height = NodeHeight(p);
+    pl.height = NodeHeight(pl);
+
+    // if the parameter passed p, is root
+    if (root == p) {
+      root = pl;
+    }
+
+    // new root
+    return pl;
+
+  }
+
+  public static Node LRRotation(Node p) {
+    // take the left child of the given node p, pl
+    // and also take the right child of (left child of p), plr
+    // make plr as center
+    // and move p towards right
+    // and distribute plr child on the right child of pl
+    // on left child of p
+    // height of plr and p changes
+    // but height of pl remains the same
+
+    Node pl = p.left;
+    Node plr = pl.right;
+
+    // arranging left child of plr, first
+    pl.right = plr.left;
+    // arranging right child of plr, second
+    p.left = plr.right;
+
+    // making plr as the center node
+    plr.left = pl;
+    plr.right = p;
+
+    // for calculating the height of the plr
+    // first calculate the height of pl and p
+    // as these are at lower level than plr
+    // and then go for plr
+    pl.height = NodeHeight(pl);
+    p.height = NodeHeight(p);
+    plr.height = NodeHeight(plr);
+
+    if (root == p) {
+      root = plr;
+    }
+
+    // return new root
+    return plr;
+
+  }
+
+  public static Node RRRotation(Node p) {
+    // taking the right child of the given node, pr
+    // taking the left child of (right child of p), prl
+    Node pr = p.right;
+    Node prl = pr.left;
+
+    // making prl the right child of p
+    p.right = prl;
+
+    // making left child of pr as p
+    pr.left = p;
+
+    // height of the prl reamins the same
+    // height of the p and pr changes
+    // first calculate prl, then p and then pr
+    p.height = NodeHeight(p);
+    pr.height = NodeHeight(pr);
+
+    // check if the given node is root
+    if (root == p) {
+      root = pr;
+    }
+
+    // return new root
+    return pr;
+  }
+
+  public static Node RLRotation(Node p) {
+    // taking the right child of the given node, pr
+    // taking the left child of (right child of given node), prl
+    Node pr = p.right;
+    Node prl = pr.left;
+
+    // arrange the childrens of prl
+    p.right = prl.left;
+    pr.left = prl.right;
+
+    // making the prl as the center
+    prl.left = p;
+    prl.right = pr;
+
+    // height of all the nodes changes
+    // changing the height of p and pr
+    p.height = NodeHeight(p);
+    pr.height = NodeHeight(pr);
+    prl.height = NodeHeight(prl);
+
+    // check for the root case
+    if (root == p) {
+      root = prl;
+    }
+
+    // return new root
+    return prl;
+
+  }
+
+  public static Node RInsert(Node root, int key) {
+    // 1. Inserting the new node
+    if (root == null) {
+      root = new Node(key);
+      return root;
+    }
+
+    if (key < root.data) {
+      root.left = RInsert(root.left, key);
+    } else if (key > root.data) {
+      root.right = RInsert(root.right, key);
+    }
+
+    // 2. Updating the height of each node
+    // at returning time we are updating the height of each node
+    root.height = NodeHeight(root);
+
+    // 3. Performing suitable rotations (at returning time)
+    if (BalanceFactor(root) == 2 && BalanceFactor(root.left) == 1) {
+      return LLRotation(root);
+    } else if (BalanceFactor(root) == 2 && BalanceFactor(root.left) == -1) {
+      return LRRotation(root);
+    } else if (BalanceFactor(root) == -2 && BalanceFactor(root.right) == -1) {
+      return RRRotation(root);
+    } else if (BalanceFactor(root) == -2 && BalanceFactor(root.right) == 1) {
+      return RLRotation(root);
+    }
+
+    return root;
+  }
+
+  public static void main(String[] args) {
+    root = RInsert(root, 50);
+    RInsert(root, 10);
+    RInsert(root, 20);
+
+    System.out.println("HELLO");
+
+  }
+
+}
