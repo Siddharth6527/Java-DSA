4d09f51a15bc764292ee60d4c7fe359b7566d042
Siddharth <siddharth6527@gmail.com>
2024-01-14 23:52:37 +0530

    Added Types of Mapping Data-Structure

---
+++ Java Basics/Threading/code/Main.java
@@ -0,0 +1,17 @@
+// public class Main extends Thread {
+// public void run() {
+// System.out.println("THis code is running in a thread");
+// }
+// }
+
+///////////////////////////////////////////
+// public class Main implements Runnable {
+// public void run() {
+// System.out.println("This code is running in a thread");
+// }
+// }
+
+///////////////////////////////////////////
+public class Main extends Thread{
+  public static void 
+}
